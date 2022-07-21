4b936c1fde9ac3cb4e761abb2b07b85a21579c32
Eugene Zhang <zhang.eug@gmail.com>
2022-07-20 22:35:32 -0400

    Add clear and delete functionality, decimal usage, and keyboard supp
    Improved error message functionality and fixed bugs.

--- script.js
+++ script.js
@@ -11,13 +11,18 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return num1 / num2; 
 }
 
 function operate(num1, num2, operator) {
     OPERATION.length = 0;
-    if (!(isNaN(num1)) && typeof operator === 'function' && (!(isNaN(num2)))) {
-        return Math.round(operator(num1, num2) * 10 ** 13) / 10 ** 13;
+    if (num2 == 0 && operator === divide) {
+        alert("Imagine trying to divide by 0 .-.");
+        return "error";
+    } else if (!(isNaN(num1)) && typeof operator === 'function' && (!(isNaN(num2)))) {
+        let solution = operator(num1, num2);
+        if (Number.isInteger(solution)) return solution;
+        return Math.round(operator(num1, num2) * 10 ** 10) / 10 ** 10;
     }
     return "error";
 }
@@ -27,13 +32,14 @@ function populateButtonDisplay(e) {
     let num = Number(e.target.innerText);
     
     if (!(isNaN(num))) {
-
         if (DISPLAYON) {
             display.innerText = "";
             DISPLAYON = false;
         }
-
         display.innerText += e.target.innerText;
+
+    } else if (e.target.innerText === ".") {
+        display.innerText += ".";
     }
 }
 
@@ -42,9 +48,9 @@ function populateAnswerDisplay(answer) {
 
     if (answer === "error") {
         display.innerText = "ERROR";
+        OPERATION.length = 0
     } else {
         display.innerText = answer;
-
     }
     DISPLAYON = true;
 }
@@ -59,7 +65,7 @@ function checkOperator(e) {
                 OPERATION.push(answer);
             } 
             OPERATION.push(divide);
-            
+            checkError();
             break;
         case 'x':
             clearOperatorDisplay();
@@ -69,7 +75,7 @@ function checkOperator(e) {
                 OPERATION.push(answer);
             } 
             OPERATION.push(multiply);
-            
+            checkError();
             break;
         case '-':
             clearOperatorDisplay();
@@ -79,7 +85,7 @@ function checkOperator(e) {
                 OPERATION.push(answer);
             } 
             OPERATION.push(subtract);
-            
+            checkError();
             break;
         case '+':
             clearOperatorDisplay();
@@ -88,9 +94,8 @@ function checkOperator(e) {
                 populateAnswerDisplay(answer);
                 OPERATION.push(answer);
             } 
-
             OPERATION.push(add);
-            
+            checkError();
             break;
         case '=':
             clearOperatorDisplay();
@@ -102,8 +107,36 @@ function checkOperator(e) {
 
 function clearOperatorDisplay() {
     const display = document.querySelector("#display-text");
-    OPERATION.push(display.innerText);
-    display.innerText = "";
+
+    if (!(display.innerText === "")) {
+        OPERATION.push(display.innerText);
+        display.innerText = "";
+    }
+}
+
+function clearClearDisplay(e) {
+    const display = document.querySelector("#display-text");
+    
+    if (e.target.innerText === "CLEAR") {
+        display.innerText = "";
+        OPERATION.length = 0;
+    } else if (e.target.innerText === "DELETE") {
+        display.innerText = display.innerText.slice(0,-1);
+    }
+}
+
+function checkError() {
+    if (typeof OPERATION[0] === "function" || typeof OPERATION[2] === "function") {
+        populateAnswerDisplay("error");
+    }
+}
+
+function displayNumber(e) {
+    const display = document.querySelector("#display-text");
+    const number = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    if (number) {
+        display.innerText += number.innerText;
+    }
 }
 
 // create an array to hold operands and operators
@@ -115,4 +148,5 @@ let DISPLAYON = false;
 const btnContainer = document.querySelector(".button-container");
 btnContainer.addEventListener('mouseup', populateButtonDisplay);
 btnContainer.addEventListener('mouseup', checkOperator);
-
+btnContainer.addEventListener('mouseup', clearClearDisplay);
+window.addEventListener('keydown', displayNumber);
